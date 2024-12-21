95021a42faf04b950adc0b1b9ead5689fe5a7812
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-12-21 18:52:55 -0300

    Aula 7_httpVerbs

---
+++ 07_HttpVerbs/rest-with-spring-boot-and-java-richard/src/main/java/br/com/richard/repositories/PersonRepository.java
@@ -0,0 +1,11 @@
+package br.com.richard.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.richard.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{}
