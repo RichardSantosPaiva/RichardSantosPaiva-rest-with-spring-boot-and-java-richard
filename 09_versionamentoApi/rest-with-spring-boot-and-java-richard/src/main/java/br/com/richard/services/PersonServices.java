933158d4d2e86f91fd915efb9d19ce51de09d2bf
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-12-26 09:49:11 -0300

    aula 09_versionamentoApi feito

--- 09_versionamentoApi/rest-with-spring-boot-and-java-richard/src/main/java/br/com/richard/services/PersonServices.java
+++ 09_versionamentoApi/rest-with-spring-boot-and-java-richard/src/main/java/br/com/richard/services/PersonServices.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.richard.data.vo.v1.PersonVO;
+import br.com.richard.data.vo.v2.PersonVOV2;
 import br.com.richard.exceptions.ResourceNotFoundException;
 import br.com.richard.mapper.DozerMapper;
+import br.com.richard.mapper.custom.PersonMapper;
 import br.com.richard.model.Person;
 import br.com.richard.repositories.PersonRepository;
 
@@ -19,6 +21,9 @@ public class PersonServices {
 
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonMapper mapper;
 	
 	public List<PersonVO> findAll(){
 		logger.info("find all people!");
@@ -36,6 +41,13 @@ public class PersonServices {
 		logger.info("creating one person!");
 		var entity =  DozerMapper.parseObject(person, Person.class);
 		var vo =  DozerMapper.parseObject(repository.save(entity),PersonVO.class);
+		return vo;
+	}
+	
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		logger.info("creating one person with V2!");
+		var entity =  mapper.convertVoToEntityToVo(person);
+		var vo =  mapper.convertEntityToVo(repository.save(entity));
 		return vo;
 	}
 	
