708328a237ce593463196b58b1d9bc91de5213ea
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-12-28 21:53:51 -0300

    adding 11 custom json serialization

---
+++ 11_CustomJsonSetilization/rest-with-spring-boot-and-java-richard/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(2, 'Brasil', 'Luan', 'Male', 'Santana'),
+	(3, 'Brasil', 'Sebastião', 'Male', 'Alisson'),
+	(4, 'Brasil', 'Fulano', 'Male', 'Ciclano'),
+	(5, 'Brasil', 'Beltrano', 'Male', 'Leandro'),
+	(6, 'Brasil', 'Sayonara', 'Male', 'Mano');
