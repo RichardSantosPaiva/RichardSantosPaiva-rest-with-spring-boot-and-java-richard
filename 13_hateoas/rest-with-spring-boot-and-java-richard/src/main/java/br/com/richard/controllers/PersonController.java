b5ba685c51e801726376cd2efaa57554e5d4fede
RichardSantosPaiva <richardpaiva0101@gmail.com>
2025-01-02 20:10:00 -0300

    testando a geração dos links HATEOAS no serviço mockito e JUNIT 5

--- 13_hateoas/rest-with-spring-boot-and-java-richard/src/main/java/br/com/richard/controllers/PersonController.java
+++ 13_hateoas/rest-with-spring-boot-and-java-richard/src/main/java/br/com/richard/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.richard.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
